fbc31c2e43c8ae7084e8801e8e65cf4bbd26e477
NyamaoGregoryKaiser <127108715+NyamaoGregoryKaiser@users.noreply.github.com>
2023-06-20 19:30:26 +0300

    Add files via upload

---
+++ Gallery.js
@@ -0,0 +1,12 @@
+// Move me to Profile.js
+import {Profile} from './Profile.js';
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
